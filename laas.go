cedc50e773482b973cdfcb46fe49251ac9c231b0
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-11-24 16:16:40 +0000

    Use duration from request

--- laas.go
+++ laas.go
@@ -8,8 +8,13 @@ import (
 
 // LaasHandler returns OK
 func LaasHandler(w http.ResponseWriter, r *http.Request) {
-	time.Sleep(500 * time.Millisecond)
-	fmt.Fprint(w, "OK")
+	duration, err := time.ParseDuration(r.FormValue("duration"))
+	if err != nil {
+		fmt.Fprint(w, err)
+	} else {
+		time.Sleep(duration)
+		fmt.Fprint(w, "OK")
+	}
 }
 
 func main() {
