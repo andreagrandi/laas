bfd1c7ea6d67a513eb64044c81d9e955bbb9ee84
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-11-24 16:08:04 +0000

    Sleep before 500ms before responding

--- laas.go
+++ laas.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 // LaasHandler returns OK
 func LaasHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(500 * time.Millisecond)
 	fmt.Fprint(w, "OK")
 }
 
